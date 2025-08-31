2de7bca15d4912d3b03a87aa349b66059d8fd2cb
ahmaddimasyqi123 <ahmaddimasyqi10@gmail.com>
2025-08-31 12:44:33 +0700

    Portfolio Ahmad Dimasyqi Yahya

---
+++ src/data.js
@@ -0,0 +1,275 @@
+import HeroImage from "/assets/img.jpg";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/php.png";
+import Tools13 from "/assets/tools/java.png";
+import Tools14 from "/assets/tools/Kotlin.png";
+import Tools15 from "/assets/tools/python.png";
+import Tools16 from "/assets/tools/C.png";
+import Tools17 from "/assets/tools/C++.png";
+import Tools18 from "/assets/tools/csharp.png";
+import Tools19 from "/assets/tools/Go.png";
+import Tools20 from "/assets/tools/Laravel.png";
+import Tools21 from "/assets/tools/codeigniter.png";
+import Tools22 from "/assets/tools/MySQL.png";
+import Tools23 from "/assets/tools/Oracle.png";
+import Tools24 from "/assets/tools/PostgreSQL.png";
+import Tools25 from "/assets/tools/MicrosoftAccess.png";
+import Tools26 from "/assets/tools/Cisco.png";
+import Tools27 from "/assets/tools/mikrotik.png";
+import Tools28 from "/assets/tools/coreldraw.png";
+import Tools29 from "/assets/tools/Sublimetext.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Illustrator",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "PHP",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Java",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "Kotlin",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "Python",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "C Language",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "C++",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "C#",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "Go Language",
+    ket: "Programming Language",
+    dad: "1100",
+  },
+  {
+    id: 20,
+    gambar: Tools20,
+    nama: "Laravel",
+    ket: "Framework",
+    dad: "1100",
+  },
+  {
+    id: 21,
+    gambar: Tools21,
+    nama: "Codeigniter",
+    ket: "Framework",
+    dad: "1100",
+  },
+  {
+    id: 22,
+    gambar: Tools22,
+    nama: "MySQL",
+    ket: "Database",
+    dad: "1100",
+  },
+  {
+    id: 23,
+    gambar: Tools23,
+    nama: "Oracle",
+    ket: "Database",
+    dad: "1100",
+  },
+  {
+    id: 24,
+    gambar: Tools24,
+    nama: "PostgreSQL",
+    ket: "Database",
+    dad: "1100",
+  },
+  {
+    id: 25,
+    gambar: Tools25,
+    nama: "Microsoft Access",
+    ket: "Database",
+    dad: "1100",
+  },
+  {
+    id: 26,
+    gambar: Tools26,
+    nama: "Cisco Packet Tracer",
+    ket: "Network Simulator",
+    dad: "1100",
+  },
+  {
+    id: 27,
+    gambar: Tools27,
+    nama: "Mikrotik",
+    ket: "Network Simulator",
+    dad: "1100",
+  },
+  {
+    id: 28,
+    gambar: Tools28,
+    nama: "CorelDRAW",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 29,
+    gambar: Tools29,
+    nama: "Sublime Text",
+    ket: "Text Editor",
+    dad: "1100",
+  },
+  
+];
+
+import Proyek1 from "/assets/proyek/proyek7.png";
+import Proyek2 from "/assets/proyek/proyek8.png";
+import Proyek3 from "/assets/proyek/proyek3.webp";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Digital Library",
+    desk: "Digilib UMK adalah perpustakaan digital Universitas Muria Kudus yang menyediakan koleksi buku, jurnal, skripsi, dan karya ilmiah secara online untuk mendukung kegiatan belajar, penelitian, dan pengembangan ilmu pengetahuan.",
+    tools: ["HTML", "CSS", "Javascript", "JSON", "XML"],
+    dad: "200",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "Website Sekolah",
+    desk: "Website resmi SMP Negeri 1 Kudus sebagai pusat informasi sekolah, media pembelajaran, dan sarana komunikasi antara guru, siswa, serta masyarakat.",
+    tools: ["HTML", "CSS", "Javascript" ],
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Web Portofolio",
+    desk: "Website portofolio yang menampilkan karya, pengalaman, dan keahlian sebagai media personal branding serta bukti profesionalisme.",
+    tools: ["Vite", "ReactJS", "TailwindCSS", "AOS"],
+    dad: "400",
+  },
+];
